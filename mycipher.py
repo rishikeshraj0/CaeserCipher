ec9b7cfa0e1c05fa0b2b290fe522f6b7f492366e
rishikeshraj0 <rmeesal1@asu.edu>
2022-02-22 10:13:11 +0530

    Modified code for decryption

--- mycipher.py
+++ mycipher.py
@@ -24,12 +24,12 @@ class CeaserCipher(unittest.TestCase):
             output=""
             for i in range(len(self.word)):
                 if ord(self.word[i])>=ord('a') and ord(self.word[i])<=ord('z'):
-                    temp=ord(self.word[i])+int(self.cipher)
+                    temp=ord(self.word[i])-int(self.cipher)
                     if temp<ord('a'):
                         temp=temp+26
                     output+=chr(temp)
                 if ord(self.word[i])>=ord('A') and ord(self.word[i])<=ord('Z'):
-                    temp=ord(self.word[i])+int(self.cipher)
+                    temp=ord(self.word[i])-int(self.cipher)
                     if temp<ord('Z'):
                         temp=temp+26
                     output+=chr(temp)
